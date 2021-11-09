db77b118668b1a703f9e1271cf31203aa2ee0434
amberDaxle <amber.khan@data-axle.com>
2021-11-09 08:50:58 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./Redux";
+import RepositoriesList from "./Components/RepositoriesList/RepositoriesList";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1>Search for a Package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
